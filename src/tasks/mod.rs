392e57d46d581e562a07eff7ef2e33e7b215f32b
Ege Apak <egeapak@gmail.com>
2025-11-05 11:14:11 +0300

    apply cargo fmt

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -1,3 +1,3 @@
 pub mod cleanup;
 
-pub use cleanup::{start_cleanup_task, start_rate_limiter_cleanup_task, cleanup_expired_sessions};
+pub use cleanup::{cleanup_expired_sessions, start_cleanup_task, start_rate_limiter_cleanup_task};
