c93ac082485d4a0f8132aee670c1dd932c692d8b
Ege Apak <egeapak@gmail.com>
2025-11-05 11:42:39 +0300

    add socketaddr extractor

--- src/main.rs
+++ src/main.rs
@@ -143,7 +143,11 @@ async fn main() -> anyhow::Result<()> {
 
     tracing::info!("Server listening on {}", config.server_address());
 
-    axum::serve(listener, app).await?;
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<std::net::SocketAddr>(),
+    )
+    .await?;
 
     Ok(())
 }
